ec507084ad4965c9ffe71e0ec639d69c5786ffdd
Paolo Cisneros <50997064+Tenfur@users.noreply.github.com>
2024-05-16 00:49:17 -0500

    first request

--- src/core/RegisterApplicationService.js
+++ src/core/RegisterApplicationService.js
@@ -4,6 +4,17 @@ class RegisterApplicationService{
     constructor(){
 
     }
+    async getScenarios(){
+        try{
+            const response =  await http.get("/scenarios")
+            const {data} = response
+            return data
+        }
+        catch(error){
+            console.log(error)
+            return error
+        }
+    }
 }
 
 export default RegisterApplicationService;
--- src/views/ScenariosView.vue
+++ src/views/ScenariosView.vue
@@ -3,6 +3,23 @@
 </template>
 <script>
 
+import RegisterApplicationService from "../core/RegisterApplicationService.js";
+
+export default {
+    data(){
+        return {
+            scenarios: []
+        }
+    },
+    methods: {
+    },
+    async created() {
+        const objService = new RegisterApplicationService();
+        const response = await objService.getScenarios()
+        console.log(response)
+    }
+}
+
 </script>
 <style>
 
